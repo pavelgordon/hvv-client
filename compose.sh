4ddd3b478fcfdc0cf2626642af8eb6bcee70483a
Pavel Gordon <gordon.pav@gmail.com>
2019-12-12 22:13:11 +0000

    Update compose.sh

--- compose.sh
+++ compose.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
 docker login registry.gitlab.com -u pavelgordon -p $DOCKER_TOKEN
-docker pull registry.gitlab.com/pavelgordon/hvvclient
-docker stop hvvclient || true
+docker pull registry.gitlab.com/pavelgordon/hvv-client
+docker stop hvv-client || true
 
-docker run --name hvvclient --rm -d \
+docker run --name hvv-client --rm -d \
 --env "VIRTUAL_HOST=pgordon.dev" \
 --env "LETSENCRYPT_HOST=pgordon.dev" \
 --env "LETSENCRYPT_EMAIL=pgordon.dev" \
-registry.gitlab.com/pavelgordon/hvvclient
+registry.gitlab.com/pavelgordon/hvv-client
